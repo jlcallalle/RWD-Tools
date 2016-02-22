7bd70a4463f5b3d41d977f1eea1217c647325e84
Jorge <jlcallalle@gmail.com>
2016-02-22 00:19:57 -0500

    se agrego menus responsive

---
+++ menu-responsive/slide-down/js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+	//alert('s');
+    $("a[href=#menuExpand]").click(function(e) {
+        $(".menu").toggleClass("menuOpen");
+        e.preventDefault();
+    });
+});
+
+jQuery(document).ready(function(){
+
+
+});
